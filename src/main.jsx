5d77862964c2e9636bb12296a8f5623341318a9f
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-08-07 21:57:21 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./pages/App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cadastro from "./pages/Cadastro.jsx";
+import Anuncios from "./pages/Anuncios.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<App />} />
+        <Route path={"/cadastro"} element={<Cadastro />} />
+        <Route path={"/anuncios"} element={<Anuncios />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ src/pages/Anuncios.jsx
@@ -0,0 +1,13 @@
+import BodyList from "../components/BodyList";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export default function Anuncios() {
+  return (
+    <div>
+      <Header />
+      <BodyList />
+      <Footer />
+    </div>
+  );
+}
