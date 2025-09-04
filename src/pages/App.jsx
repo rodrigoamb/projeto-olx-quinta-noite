e70ad48f8244493c49c3f84b6c183d3ba1cafe23
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-09-04 20:47:06 -0300

    fazendo o get

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,13 +1,39 @@
+import { useEffect, useState } from "react";
 import BodyList from "../components/BodyList";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { CircleNotchIcon } from "@phosphor-icons/react";
 
 export default function App() {
+  const [dataAnunciosList, setDataAnunciosList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function fetchDataAnuncios() {
+    try {
+      
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    console.log("QUALQUE COISA");
+  }, []);
+
   return (
     <div>
-      <Header titulo={"Todos os anúncios"} />
-      <BodyList titulo={"Todos os anúncios"} />
-      <Footer />
+      {isLoading ? (
+        <div className=" h-screen flex justify-center items-center flex-col">
+          <CircleNotchIcon size={60} className="text-[#A523FF] animate-spin" />
+          <p className="text-[#A523FF]">Carregando...</p>
+        </div>
+      ) : (
+        <>
+          <Header titulo={"Todos os anúncios"} />
+          <BodyList titulo={"Todos os anúncios"} />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
