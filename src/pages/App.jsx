33df357e44b5141f96d86a51a8941900ec915f80
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-09-04 21:02:50 -0300

    get funcionando

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,22 +3,45 @@ import BodyList from "../components/BodyList";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { CircleNotchIcon } from "@phosphor-icons/react";
+import { toast } from "react-toastify";
 
 export default function App() {
   const [dataAnunciosList, setDataAnunciosList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
-  function fetchDataAnuncios() {
+  async function fetchDataAnuncios() {
+    setIsLoading(true);
+
     try {
-      
+      const response = await fetch(
+        "https://dc-classificados.up.railway.app/api/anuncios/getAllAnuncios",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setDataAnunciosList(data);
+      } else {
+        toast.error(data.message);
+      }
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
   useEffect(() => {
-    console.log("QUALQUE COISA");
+    fetchDataAnuncios();
   }, []);
+
+  console.log(dataAnunciosList);
 
   return (
     <div>
